24e9ed067b8e1e27dce89cf0e085d207b7d1b7d8
ysakamoto <yus.sakamoto@gmail.com>
2015-03-20 11:38:51 -0500

    insert function updated

--- texteditbox.py
+++ texteditbox.py
@@ -18,7 +18,7 @@ def rectangle(win, uly, ulx, lry, lrx):
     win.addch(lry, ulx, curses.ACS_LLCORNER)
 
 
-class Textbox:
+class TextEditBox:
 
     """Editing widget using the interior of a window object.
      Supports the following Emacs-like key bindings:
@@ -49,8 +49,9 @@ class Textbox:
         self.insert_mode = insert_mode
         self.lastcmd = None
         self.text = ['']
-        self.ln = 0  # line number
-        self.cn = 0  # column number
+        self.txln = [0]
+        self.ppos = [0, 0] # physical position
+        self.vpos = [0, 0] # virtual position
         self.nlines = 1
         win.keypad(1)
 
@@ -73,36 +74,34 @@ class Textbox:
         return last
 
     def _insert_printable_char(self, ch):
+
+        if self.ppos[1] == 0:
+            self.text[self.ppos[0]] = chr(ch) + self.text[self.ppos[0]]
+        else:
+            self.text[self.ppos[0]]\
+                = self.text[self.ppos[0]][:self.ppos[1]] + chr(ch) \
+                + self.text[self.ppos[0]][self.ppos[1]:]
+        
         (y, x) = self.win.getyx()
         (maxy, maxx) = self._getmaxyx()
-        (backy, backx) = None, None
-        (backy, backx) = 0, 0
+        (backy, backx) = self.win.getyx()
+        
+        trailingstr = self.text[self.ppos[0]][self.ppos[1]+1:]
+        try:
+            self.win.addch(ch)
+        except curses.error:
+            pass
+        
+        if (self.insert_mode):
+            self.ppos[1] += 1
+            nspaces = maxx - self.ppos[1]
+            if nspaces > len(trailingstr):
+                self.win.addstr(self.ppos[0], self.ppos[1], trailingstr[:])
+            else:
+                self.win.addstr(self.ppos[0], self.ppos[1], trailingstr[:nspaces])
+                self.win.addstr(self.ppos[0]+1, 0, trailingstr[nspaces:])
+                self.ppos[0] += 1
 
-        nl = 0
-        while (y < maxy and x <= maxx):
-            oldch = self.win.inch()
-            # The try-catch ignores the error we trigger from some curses
-            # versions by trying to write into the lowest-rightmost spot
-            # in the window.
-            try:
-                self.win.addch(ch)
-            except curses.error:
-                pass
-
-            if not self.insert_mode:
-                break
-
-            # Remember where to put the cursor back since we are in insert_mode
-            if nl == 0:
-                nl = 1
-                (backy, backx) = self.win.getyx()
-
-            ch = oldch
-            (y, x) = self.win.getyx()
-            if (x == 0 and y > 0):
-                break
-
-        if self.insert_mode:
             self.win.move(backy, backx)
 
     def do_command(self, ch):
@@ -111,114 +110,110 @@ class Textbox:
         self.lastcmd = ch
 
         if curses.ascii.isprint(ch):
-            if self.ln < maxy or self.cn < maxx:
+            if self.ppos[0] < maxy or self.ppos[1] < maxx:
                 self._insert_printable_char(ch)
-                if self.cn == 0:
-                    self.text[self.ln] = chr(ch) + self.text[self.ln]
-                else:
-                    self.text[self.ln]\
-                        = self.text[self.ln][:self.cn] + chr(ch) \
-                        + self.text[self.ln][self.cn:]
-                self.cn += 1
-
+            else:
+                curses.beep()
+            self.win.move(self.ppos[0], self.ppos[1])
+                
         elif ch == curses.ascii.SOH:  # ^a
-            self.cn = 0
-            self.win.move(self.ln, self.cn)
+            self.ppos[1] = 0
+            self.win.move(self.ppos[0], self.ppos[1])
 
         elif ch == curses.ascii.ENQ:  # ^e
-            self.cn = len(self.text[self.ln])
-            self.win.move(self.ln, self.cn)
+            self.ppos[1] = len(self.text[self.ppos[0]])
+            self.win.move(self.ppos[0], self.ppos[1])
 
         elif ch in (curses.ascii.STX, curses.KEY_LEFT):
-            if self.cn > 0:
-                self.cn -= 1
-                self.win.move(self.ln, self.cn)
-            elif self.ln == 0:
+            if self.ppos[1] > 0:
+                self.ppos[1] -= 1
+                self.win.move(self.ppos[0], self.ppos[1])
+            elif self.ppos[0] == 0:
                 curses.beep()
                 pass
             else:
-                self.ln -= 1
-                self.cn = len(self.text[self.ln])
-                self.win.move(self.ln, self.cn)
+                self.ppos[0] -= 1
+                self.ppos[1] = len(self.text[self.ppos[0]])
+                self.win.move(self.ppos[0], self.ppos[1])
 
         elif ch == curses.ascii.EOT:  # ^d
-            if self.cn == len(self.text[self.ln]):
+            if self.ppos[1] == len(self.text[self.ppos[0]]):
                 curses.beep()
             else:
-                self.text[self.ln]\
-                    = self.text[self.ln][:self.cn]\
-                    + self.text[self.ln][self.cn + 1:]
+                self.text[self.ppos[0]]\
+                    = self.text[self.ppos[0]][:self.ppos[1]]\
+                    + self.text[self.ppos[0]][self.ppos[1] + 1:]
                 self.win.delch()
 
         elif ch in (curses.ascii.BS, curses.KEY_BACKSPACE, curses.ascii.DEL):
-            if self.cn == 0:
-                if self.ln == 0:
+            if self.ppos[1] == 0:
+                if self.ppos[0] == 0:
                     curses.beep()
                 else:
-                    self.cn = len(self.text[self.ln - 1])
-                    self.text[self.ln - 1] += self.text[self.ln]
-                    self.text.pop(self.ln)
+                    self.ppos[1] = len(self.text[self.ppos[0] - 1])
+                    self.text[self.ppos[0] - 1] += self.text[self.ppos[0]]
+                    self.text.pop(self.ppos[0])
                     self.nlines -= 1
-                    self.ln -= 1
-                    self.redraw_lines(self.ln, self.nlines)
-                    self.win.move(self.ln, self.cn)
+                    self.ppos[0] -= 1
+                    self.redraw_lines(self.ppos[0], self.nlines)
+                    self.win.move(self.ppos[0], self.ppos[1])
             else:
-                self.text[self.ln]\
-                    = self.text[self.ln][:self.cn - 1]\
-                    + self.text[self.ln][self.cn:]
-                self.win.move(self.ln, self.cn - 1)
+                self.text[self.ppos[0]]\
+                    = self.text[self.ppos[0]][:self.ppos[1] - 1]\
+                    + self.text[self.ppos[0]][self.ppos[1]:]
+                self.win.move(self.ppos[0], self.ppos[1] - 1)
                 self.win.delch()
-                self.cn -= 1
+                self.ppos[1] -= 1
 
         elif ch in (curses.ascii.ACK, curses.KEY_RIGHT):  # ^f
-            if self.cn < len(self.text[self.ln]):
-                self.cn += 1
-                self.win.move(self.ln, self.cn)
-            elif (self.ln == maxy) | (self.ln == self.nlines - 1):
+            if self.ppos[1] < len(self.text[self.ppos[0]]):
+                self.ppos[1] += 1
+                self.win.move(self.ppos[0], self.ppos[1])
+            elif (self.ppos[0] == maxy) | (self.ppos[0] == self.nlines - 1):
                 curses.beep()
                 pass
             else:
-                self.cn = 0
-                self.ln += 1
-                self.win.move(self.ln, 0)
+                self.ppos[1] = 0
+                self.ppos[0] += 1
+                self.win.move(self.ppos[0], 0)
 
         elif ch in [curses.ascii.NL, curses.ascii.SI]:  # ^j, ^o
             if maxy == 0:
                 curses.beep()
                 return 0
-            elif self.ln < maxy:
-                self.text.insert(self.ln + 1, self.text[self.ln][self.cn:])
-                self.text[self.ln] = self.text[self.ln][:self.cn]
-                self.ln += 1
-                self.cn = 0
+            elif self.ppos[0] < maxy:
+                self.text.insert(self.ppos[0] + 1, self.text[self.ppos[0]][self.ppos[1]:])
+                self.text[self.ppos[0]] = self.text[self.ppos[0]][:self.ppos[1]]
+                self.ppos[0] += 1
+                self.ppos[1] = 0
                 self.nlines += 1
-                self.redraw_lines(self.ln - 1, self.nlines)
-                self.win.move(self.ln, 0)
+                self.redraw_lines(self.ppos[0] - 1, self.nlines)
+                self.win.move(self.ppos[0], 0)
 
         elif ch == curses.ascii.VT:  # ^k
-            for c in range(self.cn, len(self.text[self.ln])):
+            for c in range(self.ppos[1], len(self.text[self.ppos[0]])):
                 self.win.addch(' ')
-            self.text[self.ln]\
-                = self.text[self.ln][:self.cn]
-            self.cn = len(self.text[self.ln])
-            self.win.move(self.ln, self.cn)
+            self.text[self.ppos[0]]\
+                = self.text[self.ppos[0]][:self.ppos[1]]
+            self.ppos[1] = len(self.text[self.ppos[0]])
+            self.win.move(self.ppos[0], self.ppos[1])
             
         elif ch == curses.ascii.FF:  # ^l
             self.win.refresh()
 
         elif ch in (curses.ascii.SO, curses.KEY_DOWN):  # ^n
-            if self.ln < (self.nlines - 1):
-                self.ln += 1
-                self.cn = min(self.cn, len(self.text[self.ln]))
-                self.win.move(self.ln, self.cn)
+            if self.ppos[0] < (self.nlines - 1):
+                self.ppos[0] += 1
+                self.ppos[1] = min(self.ppos[1], len(self.text[self.ppos[0]]))
+                self.win.move(self.ppos[0], self.ppos[1])
             else:
                 curses.beep()
 
         elif ch in (curses.ascii.DLE, curses.KEY_UP):  # ^p
-            if self.ln > 0:
-                self.ln -= 1
-                self.cn = min(self.cn, len(self.text[self.ln]))
-                self.win.move(self.ln, self.cn)
+            if self.ppos[0] > 0:
+                self.ppos[0] -= 1
+                self.ppos[1] = min(self.ppos[1], len(self.text[self.ppos[0]]))
+                self.win.move(self.ppos[0], self.ppos[1])
             else:
                 curses.beep()
 
@@ -249,13 +244,13 @@ class Textbox:
             if not self.do_command(ch):
                 break
 
-            (backy, backx) = self.win.getyx()
-            maxy, maxx = self._getmaxyx()
-            self.win.addstr(maxy - 2, maxx - 20, '%d %d %d'
-                            % (ch, self.ln, self.cn))
-            self.win.refresh()
+            # (backy, backx) = self.win.getyx()
+            # maxy, maxx = self._getmaxyx()
+            # self.win.addstr(maxy - 2, maxx - 20, '%d %d %d'
+                            # % (ch, self.ppos[0], self.ppos[1]))
+            # self.win.refresh()
 
-            self.win.move(backy, backx)
+            # self.win.move(backy, backx)
 
         return self.text
 
@@ -285,6 +280,7 @@ if __name__ == '__main__':
         ymax, xmax = stdscr.getmaxyx()
 
         ncols, nlines = xmax - 5, ymax - 3
+        ncols, nlines = 20, 20
         uly, ulx = 2, 2
         stdscr.addstr(uly - 2, ulx, "Use Ctrl-G to end editing.")
         win = curses.newwin(nlines, ncols, uly, ulx)
@@ -292,7 +288,7 @@ if __name__ == '__main__':
         stdscr.refresh()
 
         try:
-            out = Textbox(win, stdscr).edit(validate=validate)
+            out = TextEditBox(win, stdscr).edit(validate=validate)
         except EscapePressed:
             out = None
 
